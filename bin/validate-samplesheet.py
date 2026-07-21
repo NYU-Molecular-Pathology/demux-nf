3c391c6c79fe914e956b8296fbebabfae8ecfc64
SummerYYY96 <summeryang96@gmail.com>
2026-07-21 15:20:55 -0400

    updated to align with new bclconvert samplehsheet class in util

--- bin/validate-samplesheet.py
+++ bin/validate-samplesheet.py
@@ -4,7 +4,7 @@
 Runs validation checking on the provided Illumina samplesheet file for bcl2fastq
 """
 import sys
-from util import samplesheet
+from util import samplesheet_bclconvert
 samplesheet_file = sys.argv[1]
-sheet_obj = samplesheet.IEMFile(path = samplesheet_file)
+sheet_obj = samplesheet_bclconvert.BCLConvertFile(path = samplesheet_file)
 sheet_obj.isValid(_raise = True)
